d0f17f801a322300dcd393d1dda57ba5a9dedb5e
Brittain Hard <brittainhard@gmail.com>
2017-05-16 15:03:52 -0500

    Added folder argument.

--- api_report.py
+++ api_report.py
@@ -1,18 +1,18 @@
 from __future__ import absolute_import
 import os, subprocess, sys, shlex
 
-from bokeh.util.api_crawler import api_crawler, differ
+from api_crawler import api_crawler, differ
 
 
-def diff_versions(old_version, new_version):
-    subprocess.call(shlex.split("git checkout tags/%s -- bokeh" % old_version), stdout=open(os.devnull, "w"))
-    old = api_crawler("bokeh").get_crawl_dict()
-    subprocess.call(shlex.split("git checkout tags/%s -- bokeh" % new_version), stdout=open(os.devnull, "w"))
-    new = api_crawler("bokeh").get_crawl_dict()
+def diff_versions(old_version, new_version, folder):
+    subprocess.call(shlex.split("git checkout tags/%s -- %s" % (old_version, folder)), stdout=open(os.devnull, "w"))
+    old = api_crawler(folder).get_crawl_dict()
+    subprocess.call(shlex.split("git checkout tags/%s -- %s" % (new_version, folder)), stdout=open(os.devnull, "w"))
+    new = api_crawler(folder).get_crawl_dict()
 
     # Reset HEAD to initial state.
-    subprocess.call(shlex.split("git checkout HEAD -- bokeh"), stdout=open(os.devnull, "w"))
-    subprocess.call(shlex.split("git reset HEAD -- bokeh"), stdout=open(os.devnull, "w"))
+    subprocess.call(shlex.split("git checkout HEAD -- %s" % folder), stdout=open(os.devnull, "w"))
+    subprocess.call(shlex.split("git reset HEAD -- %s" % folder), stdout=open(os.devnull, "w"))
 
     # Combine items removed and added into a single text file.
     diff = differ(old, new)
@@ -22,7 +22,7 @@ def diff_versions(old_version, new_version):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) >= 3:
-        sys.stdout.write(diff_versions(sys.argv[1], sys.argv[2]))
+    if len(sys.argv) >= 4:
+        sys.stdout.write(diff_versions(sys.argv[1], sys.argv[2], sys.argv[3]))
     else:
-        print("Please provide an old version and a new version.")
+        print("Please provide an old version, a new version, and a folder to crawl.")
